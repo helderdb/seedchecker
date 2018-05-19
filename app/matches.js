8dfed62dab729a7be69975eea641fe046d44c383
helderdb <helderdebaere@hotmail.com>
2018-05-19 15:46:11 +0200

    shorten paths

--- app/matches.js
+++ app/matches.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 function match() {
     var inputFile = require('./input.json');
     var wordsFile = require('./words.json');
-    var input = JSON.parse(fs.readFileSync('/Users/helderdebaere/seedGen/app/input.json', 'utf8'));
-    var words = JSON.parse(fs.readFileSync('/Users/helderdebaere/seedGen/app/words.json', 'utf8'));
+    var input = JSON.parse(fs.readFileSync('./app/input.json', 'utf8'));
+    var words = JSON.parse(fs.readFileSync('./app/words.json', 'utf8'));
     console.log(input);
     var result = [];
     input.forEach(inp => {
