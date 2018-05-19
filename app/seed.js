d8994e4680543311b4ec644ce5382761009cf84f
helderdb <helderdebaere@hotmail.com>
2018-05-19 16:51:00 +0200

    Searching for a segwit P2SH address

--- app/seed.js
+++ app/seed.js
@@ -1,42 +1,42 @@
 var fs = require('fs');
 var bip39 = require('bip39');
 var bitcoin = require('bitcoinjs-lib');
-const https = require('https');
+var request = require('request');
+var addressesToSearch = [
+    "3CcxyPhyvyc3S9UuPfu42GNZLvVVV11Uk8"
+];
+var input = JSON.parse(fs.readFileSync('./app/SeedInput.json', 'utf8'));
 
 function genSeeds() {
-    var input = JSON.parse(fs.readFileSync('./app/SeedInput.json', 'utf8'));
     var results = [];
-    var teststringarray = [];
-    var testString = "";
-    var lastWord = "";
-    var totalSeeds = 0;
+    var seedArray = [];
 
     // Currently only 12 words
     for (var i = 0; i < input.length; i++) {
-        teststringarray[0] = input[i];
+        seedArray[0] = input[i];
         for (var j = 0; j < input.length; j++) {
-            teststringarray[1] = input[j];
+            seedArray[1] = input[j];
             for (var k = 0; k < input.length; k++) {
-                teststringarray[2] = input[k];
+                seedArray[2] = input[k];
                 for (var l = 0; l < input.length; l++) {
-                    teststringarray[3] = input[l];
+                    seedArray[3] = input[l];
                     for (var m = 0; m < input.length; m++) {
-                        teststringarray[4] = input[m];
+                        seedArray[4] = input[m];
                         for (var n = 0; n < input.length; n++) {
-                            teststringarray[5] = input[n];
+                            seedArray[5] = input[n];
                             for (var o = 0; o < input.length; o++) {
-                                teststringarray[6] = input[o];
+                                seedArray[6] = input[o];
                                 for (var p = 0; p < input.length; p++) {
-                                    teststringarray[7] = input[p];
+                                    seedArray[7] = input[p];
                                     for (var q = 0; q < input.length; q++) {
-                                        teststringarray[8] = input[q];
+                                        seedArray[8] = input[q];
                                         for (var r = 0; r < input.length; r++) {
-                                            teststringarray[9] = input[r];
+                                            seedArray[9] = input[r];
                                             for (var s = 0; s < input.length; s++) {
-                                                teststringarray[10] = input[s];
+                                                seedArray[10] = input[s];
                                                 for (var t = 0; t < input.length; t++) {
-                                                    teststringarray[11] = input[t];
-                                                    validateWords(input, teststringarray, results);
+                                                    seedArray[11] = input[t];
+                                                    validateWords(seedArray, results);
                                                 }
                                             }
                                         }
@@ -54,26 +54,32 @@ function genSeeds() {
     }
 }
 
-function validateWords(input, words, results) {
-    var testString = words[0];
+function validateWords(words, results) {
+    var seed = words[0];
+
     for (var i = 1; i < 12; i++) {
-        testString += " " + words[i];
+        seed += " " + words[i];
     }
-    if (bip39.validateMnemonic(testString, input)) {
-        results.push(testString);
+
+    // Clues should come here
+    if (bip39.validateMnemonic(seed, input)) {
+
         var bitcoinNetwork = bitcoin.networks.bitcoin;
-        var hdMaster = bitcoin.HDNode.fromSeedBuffer(bip39.mnemonicToSeed(testString), bitcoinNetwork); // seed from above
+        var hdMaster = bitcoin.HDNode.fromSeedBuffer(bip39.mnemonicToSeed(seed), bitcoinNetwork); // seed from above
 
-        var key1 = hdMaster.derivePath('0');
-        var addr = key1.keyPair.toWIF();
-        console.log(addr);
+        var key1 = hdMaster.derivePath("m/0'/0/0");
+        var addrWIF = key1.keyPair.toWIF();
+        var keyPair = bitcoin.ECPair.fromWIF(addrWIF);
+        var pubKey = keyPair.getPublicKeyBuffer();
 
-        // check if there's anything on the first address of the seed
-
-        // https.get('https://blockchain.info/q/addressfirstseen/' + addr, (res) => {
-        //     console.log(res);
-        // });
-        // console.log("\n\nfound seed: " + testString);
+        var redeemScript = bitcoin.script.witnessPubKeyHash.output.encode(bitcoin.crypto.hash160(pubKey));
+        var scriptPubKey = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(redeemScript));
+        var address = bitcoin.address.fromOutputScript(scriptPubKey);
+        // console.log(address);
+        if (addressesToSearch.includes(address)) {
+            console.log("Found a seed for address: " + address + " Seed: " + seed);
+            results.push(seed);
+        }
     }
 }
 
